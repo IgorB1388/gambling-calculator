a9baa51f6c14cf3008bb3955ac1ca47fe30808e6
IgorB1388 <igor.buslakov@gmail.com>
2026-02-09 15:21:04 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -734,7 +734,7 @@ async function calculateEquity() {
     isCalculating = true;
     document.getElementById('calculateBtn').disabled = true;
     
-    const SIMULATIONS = 10000;
+    const SIMULATIONS = 25000;
     let heroWins = 0, opponentWins = 0, ties = 0;
     
     const createInitialDeck = () => {
@@ -868,4 +868,5 @@ document.addEventListener('keydown', e => {
             if (e.key >= '1' && e.key <= '9') setOpponents(parseInt(e.key));
     }
 });
+
 
