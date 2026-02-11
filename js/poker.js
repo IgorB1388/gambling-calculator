d3f4b3a5ae2ab8902762a8e0c6b0363ddfba4f03
IgorB1388 <igor.buslakov@gmail.com>
2026-02-11 14:25:55 +0100

    Update poker.js

--- js/poker.js
+++ js/poker.js
@@ -935,5 +935,6 @@ if (Math.abs(100 - sum) > 0.01) {
 }
 
 document.getElementById('resultHero').textContent = heroRounded.toFixed(1) + '%';
-document.getElementById('resultOpponent').textContent = oppRounded.toFixed(1) + '%';  // 👈 ИСПРАВЛЕНО!
+document.getElementById('resultOpponent').textContent = oppRounded.toFixed(1) + '%';
 document.getElementById('resultTie').textContent = tieRounded.toFixed(1) + '%';
+
